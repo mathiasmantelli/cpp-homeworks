bcb698e66c5ca72c19202fb3a8bddf9508fcf48a
Mathias Mantelli <mathiasfassini@gmail.com>
2020-10-28 14:06:16 -0300

    Now my CMakeLists supports the 'make install' command. It's necessary to run ./install.sh first. The task2 is complete.

--- homework_1/task_2/build.sh
+++ homework_1/task_2/build.sh
@@ -5,10 +5,19 @@ echo "This is the second one, it will run after the first one"
 mkdir build
 c++ -c -I include/ src/sum.cpp -o build/sum.o
 c++ -c -I include/ src/subtract.cpp -o build/subtract.o
+c++ -c -I include/ src/main.cpp -o build/main.o
 
 ar rcs build/libipb_arithmetic.a build/sum.o build/subtract.o
 
-c++ src/main.cpp -I include/ -L build/ -lipb_arithmetic -o results/bin/main
+c++ src/main.cpp -I include/ -L build/ -lipb_arithmetic -o results/bin/test_ipb_arithmetic
 
-mv build/libipb_arithmetic.a results/lib/
+cp build/libipb_arithmetic.a results/lib/
 
+mkdir install
+cd install 
+mkdir lib
+cd ..
+
+cp -r include install/
+cp build/libipb_arithmetic.a install/lib/
+cp results/bin/test_ipb_arithmetic build/
---
+++ homework_1/task_2/install.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+cd install
+mkdir bin
+cd ..
+cp results/bin/* install/bin/
